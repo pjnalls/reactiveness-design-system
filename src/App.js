3c69bdcba0118d69dc13a0b40e3c2b879b74b543
pjnalls <pjnalls.js@gmail.com>
2021-11-23 03:19:11 -0600

    Add simple & composite components to `App`.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Title from "./components/Title";
+import Form from "./components/Form/Form";
+import TodoList from "./components/Todos/TodoList";
 
 const App = () => {
   return (
     <div className="App">
       <Title brand=".⁂." appType="TODOs"></Title>
+      <TodoList></TodoList>
+      <Form></Form>
     </div>
   );
 };
