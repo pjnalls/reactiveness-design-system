b393ded3f92cc2628ed8d861956760de2f8f6325
pjnalls <pjnalls.js@gmail.com>
2021-11-24 04:17:18 -0600

    Add mock data `TODOS`.

---
+++ src/data/Todos.js
@@ -0,0 +1,18 @@
+export const TODOS = [
+  {
+    id: 1,
+    text: "Make to-do list.",
+    day: "11/17 @ 7:00 AM",
+    top: true,
+  },
+  {
+    id: 2,
+    text: "Eat lunch.",
+    day: "11/18 @ 12:00 PM",
+  },
+  {
+    id: 3,
+    text: "Doctor's appointment.",
+    day: "11/19 @ 3:45 PM",
+  },
+];
