8e16529bf07046bb008e524a0a328d1350175293
pjnalls <pjnalls.js@gmail.com>
2021-11-24 04:13:31 -0600

    Add the hook `useState` at the `App` level.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import Title from "./components/Title";
 import Form from "./components/Form/Form";
 import TodoList from "./components/Todos/TodoList";
+import { TODOS } from "./data/Todos";
+import { useState } from "react";
 
 const App = () => {
+  const [todos, setTodos] = useState(TODOS);
+
+  const deleteTodo = (todo) => {
+    setTimeout(() => {
+      setTodos(todos.filter((t) => t.id !== todo.id));
+    }, 1000);
+  };
+
   return (
     <div className="App">
       <Title brand=".⁂." appType="TODOs"></Title>
-      <TodoList></TodoList>
+      <TodoList todos={todos} onDelete={deleteTodo}></TodoList>
       <Form></Form>
     </div>
   );
