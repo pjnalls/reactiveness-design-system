612ff932771d77653c12213381a6a15e4f302283
pjnalls <pjnalls.js@gmail.com>
2021-11-24 04:16:57 -0600

    Add `TodoList` component and styling.

--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -1,7 +1,14 @@
 import "./TodoList.scss";
+import TodoItem from "./TodoItem";
 
-const TodoList = () => {
-  return <div className="TodoList"></div>;
+const TodoList = ({ todos, onDelete }) => {
+  return (
+    <div className="TodoList">
+      {todos.map((todo, index) => (
+        <TodoItem key={index} todo={todo} onDelete={onDelete}></TodoItem>
+      ))}
+    </div>
+  );
 };
 
 export default TodoList;
